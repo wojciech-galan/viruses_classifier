2127918a220f4421eda6852f7012a5c043b74cfb
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2016-10-04 08:33:12 +0200

    initial commit

---
+++ classify.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" This file is a part of an application named enc-genetic-codes
+    If you have any questions and/or comments, don't hesitate to 
+    contact me by email wojciech.galan@gmail.com
+    
+    Copyright (C) 2015  Wojciech Gałan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>"""
+
+#todo nazwa do poprawy
+
+import argparse
+
+from sklearn.externals import joblib
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='') #todo dodać opis
+    parser.add_argument('classifier', type=int)
+    args = parser.parse_args()
+    print args
