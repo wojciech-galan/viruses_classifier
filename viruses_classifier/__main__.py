5861b41e33d68cbab2e5fc0b829e345a1e2e26e0
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-02-01 16:18:54 +0100

    seems to work fine

--- viruses_classifier/__main__.py
+++ viruses_classifier/__main__.py
@@ -15,15 +15,20 @@ def main(args=sys.argv[1:]):
     parser.add_argument('sequence', type=str, help='sequence in plaintekst')
     parser.add_argument('nucleic_acid', type=str, help='nucleic acid: either DNA or RNA')
     parser.add_argument('classifier', type=str, help='classifier: SVM, kNN or QDA')
-    parser.add_argument('--probas', action='store_true')
-    args = parser.parse_args(args)
-    if not (args.classifier.lower() == 'svm' or args.classifier.lower() == 'knn' or args.classifier.lower() == 'qda'):
+    parser.add_argument('--probas', '-p', dest='probas', action='store_true')
+    parsed_args = parser.parse_args(args)
+    if not (parsed_args.classifier.lower() == 'svm' or parsed_args.classifier.lower() == 'knn' or
+                    parsed_args.classifier.lower() == 'qda'):
         raise ValueError("Classifier should be SVM, kNN or QDA")
-    if not (args.nucleic_acid.lower() == 'dna' or args.nucleic_acid.lower() == 'rna'):
+    if not (parsed_args.nucleic_acid.lower() == 'dna' or parsed_args.nucleic_acid.lower() == 'rna'):
         raise ValueError("Nucleic acid tye should be either DNA or RNA")
-    classify('AGATA', 'dna', None, None)
-    classifier = joblib.load(os.path.join())
-    scaller = joblib.load()
+    scaler_path = os.path.join(constants.DIR_PATH, constants.CONFIG['scaler_path'])
+    classifier_path = os.path.join(constants.DIR_PATH,
+                                  constants.CONFIG['classifier_paths'][parsed_args.classifier.lower()])
+    scaler = joblib.load(scaler_path)
+    classifier = joblib.load(classifier_path)
+    print classify(parsed_args.sequence, parsed_args.nucleic_acid.lower(), scaler, classifier, parsed_args.probas)
+
 
 if __name__ == '__main__':
     main()
