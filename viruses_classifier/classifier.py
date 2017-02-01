7a6caaa3f6cd2c4c053cfc44257c433fbcb9f6cc
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-02-01 15:13:18 +0100

    classifier now should be run from __main__ file

--- viruses_classifier/classifier.py
+++ viruses_classifier/classifier.py
@@ -37,19 +37,3 @@ def classify(seq, nuc_acid, scaller, classifier, probas=False):
     if probas:
         return classifier.predict_probas
     return classifier.predict
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='') #todo dodać opis
-    parser.add_argument('sequence', type=str, help='sequence in plaintekst')
-    parser.add_argument('nucleic_acid', type=str, help='nucleic acid: either DNA or RNA')
-    parser.add_argument('classifier', type=str, help='classifier: SVM, kNN or QDA')
-    parser.add_argument('--probas', type=bool, action='store_true')
-    args = parser.parse_args()
-    if not (args.classifier.lower() == 'svm' or args.classifier.lower() == 'knn' or args.classifier.lower() == 'qda'):
-        raise ValueError("Classifier should be SVM, kNN or QDA")
-    if not (args.nucleic_acid.lower() == 'dna' or args.nucleic_acid.lower() == 'rna'):
-        raise ValueError("Nucleic acid tye should be either DNA or RNA")
-    classify('AGATA', 'dna', None, None)
-    classifier = joblib.load(os.path.join())
-    scaller = joblib.load()
