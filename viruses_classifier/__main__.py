2ad447da1f5c1dfa8e8949be61d16f263648839a
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-02-01 15:11:42 +0100

    parsing  comand line args works

--- viruses_classifier/__main__.py
+++ viruses_classifier/__main__.py
@@ -2,12 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import argparse
+from sklearn.externals import joblib
+
+import constants
 from classifier import classify
 
 
-def main(args=sys.argv):
-    print "udalo sie"
+def main(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='') #todo dodać opis
+    parser.add_argument('sequence', type=str, help='sequence in plaintekst')
+    parser.add_argument('nucleic_acid', type=str, help='nucleic acid: either DNA or RNA')
+    parser.add_argument('classifier', type=str, help='classifier: SVM, kNN or QDA')
+    parser.add_argument('--probas', action='store_true')
+    args = parser.parse_args(args)
+    if not (args.classifier.lower() == 'svm' or args.classifier.lower() == 'knn' or args.classifier.lower() == 'qda'):
+        raise ValueError("Classifier should be SVM, kNN or QDA")
+    if not (args.nucleic_acid.lower() == 'dna' or args.nucleic_acid.lower() == 'rna'):
+        raise ValueError("Nucleic acid tye should be either DNA or RNA")
+    classify('AGATA', 'dna', None, None)
+    classifier = joblib.load(os.path.join())
+    scaller = joblib.load()
 
 if __name__ == '__main__':
     main()
