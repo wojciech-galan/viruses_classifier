f35cc3523f34bdfc92ce887e098e51900cfa5526
Wojciech Galan <wojciech.galan@uj.edu.pl>
2017-02-20 10:29:46 +0100

    changed function get_feature_indices to semiprivate

--- viruses_classifier/libs/get_feature_indices.py
+++ viruses_classifier/libs/get_feature_indices.py
@@ -3,5 +3,5 @@
 
 import numpy as np
 
-def get_feature_indices(all_features, specific_features):
+def _get_feature_indices(all_features, specific_features):
     return np.array([all_features.index(feat) for feat in specific_features])
