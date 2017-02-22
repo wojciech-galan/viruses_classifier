3de459968b0fc4c5113cf9dfd670639c9b45a9fd
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-02-22 18:32:31 +0100

    more verbose output - class description, not only number

--- viruses_classifier/classifier.py
+++ viruses_classifier/classifier.py
@@ -6,6 +6,15 @@ import numpy as np
 import constants
 from libs import sequence_processing
 
+def probas_to_dict(probas, translation_dict):
+    """
+    Transtorms vector of probabilities to dictionary {"type of virus":probability, ...}
+    :param probas:
+    :param translation_dict:
+    :return:
+    """
+    return {translation_dict[i]:probas[i] for i in range(len(probas))}
+
 def classify(seq, nuc_acid, scaller, classifier, feature_indices, probas=False):
     """
     Classify viral sequence
@@ -35,5 +44,6 @@ def classify(seq, nuc_acid, scaller, classifier, feature_indices, probas=False):
     vals = scaller.transform(np.array(vals).reshape(1, -1))[:, feature_indices]
     print type(vals), vals # TODO remove
     if probas:
-        return classifier.predict_proba(vals)[0]
-    return classifier.predict(vals)[0]
+        clf_val = classifier.predict_proba(vals)[0]
+        return probas_to_dict(clf_val, constants.NUM_TO_CLASS)
+    return constants.NUM_TO_CLASS[classifier.predict(vals)[0]]
--- viruses_classifier/constants.py
+++ viruses_classifier/constants.py
@@ -12,6 +12,7 @@ from libs.get_feature_indices import _get_feature_indices
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 CONFIG = json.load(open(os.path.join(DIR_PATH, 'conf.json')))
 ACID_TO_NUMBER = {'dna':1.0, 'rna':0.0}
+NUM_TO_CLASS = {0:'Eucaryota-infecting', 1:'phage'}
 feature_indices = {
     'qda':_get_feature_indices(pickle.load(open(os.path.join(DIR_PATH, 'files', CONFIG['all_features_file']))),
                               json.load(open(os.path.join(DIR_PATH, 'files', CONFIG['features_file'])))['qda']),
