e257785d5512e876b6e2e3e80999e101aefcfbb7
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-02-22 18:17:00 +0100

    m

--- viruses_classifier/libs/read_sequence.py
+++ viruses_classifier/libs/read_sequence.py
@@ -8,8 +8,8 @@ import re
 
 class ReadSequenceException(IOError):
 
-    def __init__(self, message=
-    "Make sure your file contains either raw or FASTA-formatted sequence"):
+    def __init__(self, message="Make sure your file contains either \
+    raw or FASTA-formatted sequence"):
         super(ReadSequenceException, self).__init__(message)
 
 
