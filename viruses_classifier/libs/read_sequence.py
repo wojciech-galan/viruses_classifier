4798f50076e21707266c2dcda6def26f03c92b0f
Wojciech Gałan <wojciech.galan@uj.edu.pl>
2017-02-22 17:24:13 +0100

    docstring to read_raw

--- viruses_classifier/libs/read_sequence.py
+++ viruses_classifier/libs/read_sequence.py
@@ -1,8 +1,14 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+import re
 
 """Routines for reading nucleotide sequences in different formats"""
 
 
 def read_raw(path):
+    """
+    Reads file containing raw sequence
+    :param path: path to the file
+    :return: sequence string
+    """
     return ''.join(open(path).read().split()).upper()
