5f445447cd64e30c3066aa409a1df4431ba6f67e
wojciech-galan <wojciech.galan@gmail.com>
2017-02-26 09:58:34 +0100

    docstring

--- viruses_classifier/libs/get_feature_indices.py
+++ viruses_classifier/libs/get_feature_indices.py
@@ -4,4 +4,10 @@
 import numpy as np
 
 def _get_feature_indices(all_features, specific_features):
+    """
+    Returns indices of some specific features in list containing all the features
+    :param all_features:
+    :param specific_features:
+    :return:
+    """
     return np.array([all_features.index(feat) for feat in specific_features])
