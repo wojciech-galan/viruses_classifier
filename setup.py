902287e2aae3d2245ca29701b7b4e06283e18ed8
wojciech-galan <wojciech.galan@gmail.com>
2023-05-21 19:19:58 +0200

    fixed setup.py - finally installs proper versions od dependencies

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="viruses_classifier",
-    version='1.0.2',
+    version='1.0.3',
     description='Predict host of a virus based on its (possibly complete) genomic sequence',
     url='https://github.com/wojciech-galan/viruses_classifier',
     author='Wojciech Gałan',
     license='GNU GPL v3.0',
     install_requires=[
-        'sklearn',
-        'numpy',
-        'scipy'
+        'scikit-learn==0.19.2',
+        'numpy==1.13.3',
+        'scipy==0.13.3'
     ],
     packages=find_packages(),
     classifiers=[
@@ -22,8 +22,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7'
     ],
-    entry_points = {
-        'console_scripts':[
+    entry_points={
+        'console_scripts': [
             'viruses_classifier = viruses_classifier.__main__:main'
         ]
 
